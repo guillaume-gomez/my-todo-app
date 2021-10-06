a6193208faac745a174ea6c75f862a5db8b4086d
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-10-06 21:20:54 +0200

    add objective controller + remove association with project for the moment

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'todolist#index'
+  root to: 'objectives#index'
+
+  resource :objectives, only:[:index, :create, :update, :destroy]
+  
 end
