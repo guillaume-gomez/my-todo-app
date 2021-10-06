a9ba5502ec8cc6ab3ab3b34a05308c56e3edc038
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-10-06 21:55:42 +0200

    move default route + create objectives#index

--- app/controllers/objectives_controller.rb
+++ app/controllers/objectives_controller.rb
@@ -1,4 +1,9 @@
 class ObjectivesController < ApplicationController
   def index
+    respond_to do |format|
+      format.json do 
+        render json: Objective.all
+      end
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'objectives#index'
+  root to: 'application#index'
 
-  resource :objectives, only:[:index, :create, :update, :destroy]
+  resources :objectives, only:[:index, :create, :update, :destroy]
   
 end
