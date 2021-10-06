dde86656f467fb5f27e279b24f8ed2546cc356df
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-10-06 21:34:19 +0200

    call objectives#index endpoint

--- app/javascript/packs/application.tsx
+++ app/javascript/packs/application.tsx
@@ -1,7 +1,23 @@
-import React, { ReactElement } from "react";
-import ReactDOM from 'react-dom'
+import React, { ReactElement, useState, useEffect } from "react";
+import ReactDOM from 'react-dom';
+import axios from 'axios';
+
+import { Objectives } from "./interfaces";
+
 
 function Application(): ReactElement {
+  const [objectives, setObjectives] = useState<Objectives[]>([]);
+  useEffect(() => {
+    axios.get(`/objectives.json`)
+      .then((response) => {
+        const { data } = response;
+        setObjectives(data);
+      })
+      .catch((error) => {
+        // TODO display error
+      });
+  }, [setObjectives])
+
   return (
     <div>
       My Todo App
---
+++ app/javascript/packs/interfaces.ts
@@ -0,0 +1,3 @@
+interface ObjectivesInterface {
+  title: string;
+}
