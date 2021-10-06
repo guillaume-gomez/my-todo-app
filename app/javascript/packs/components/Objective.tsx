fd133c08ba0ace35569f7973ec2201991d4bf5fa
Guillaume Gomez <guillaume.gomez846@gmail.com>
2021-10-06 23:07:02 +0200

    make call api to update or create objectives. Use optimistic strategy

--- app/javascript/packs/components/Objective.tsx
+++ app/javascript/packs/components/Objective.tsx
@@ -1,6 +1,8 @@
 import React, { ReactElement, useState, useEffect } from "react";
+import axios from 'axios';
 import { ObjectiveInterface } from "../interfaces";
 import Button from "./Button";
+import Error from "./Error";
 
 interface ObjectiveProps {
   objective: ObjectiveInterface;
@@ -9,14 +11,41 @@ interface ObjectiveProps {
 
 function Objective({objective, onChangeTitle} : ObjectiveProps ): ReactElement {
   const [isEditing, setIsEditing] = useState<boolean>(false);
+  const [networkError, setNetworkError] = useState<string|null>(null);
   
   function onBlur(event) {
-    onChangeTitle({ ...objective, title: event.target.value });
+    const editedObjective = { ...objective, title: event.target.value };
+    onChangeTitle(editedObjective);
     setIsEditing(false);
+
+    updateOnDatabase(editedObjective);
+  }
+
+  function updateOnDatabase(objectiveToSend: ObjectiveInterface) {
+    if(!objectiveToSend.id) {
+      // make a create API call
+      axios.post(`/objectives.json`, objectiveToSend)
+      .then((response) => {
+        setNetworkError(null)
+      })
+      .catch(({message}) => {
+        setNetworkError(message)
+      });
+    } else {
+      // make an edit API call
+      axios.patch(`/objectives/${objectiveToSend.id}.json?`, objectiveToSend)
+      .then((response) => {
+        setNetworkError(null)
+      })
+      .catch(({message}) => {
+        setNetworkError(message)
+      });
+    }
   }
 
   return (
     <div style={{border: "1px solid black", padding: "6px"}}>
+      {networkError ? <Error message={networkError} /> : null}
       <div style={{display: "flex", justifyContent:"space-between"}}>
         {isEditing ?
           <input type="text" defaultValue={objective.title} onBlur={onBlur}/> :
